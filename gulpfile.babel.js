39a389822216ec6cec893f5d4e251f85b10f88fa
Arkadiy_vl <arkadiy_vl@inbox.ru>
2018-05-27 01:15:22 +0500

    * package.jsonб *gulpfile.babel.json (доработка под себя, использование sass) + main.sass, + _reset.sass (переход на sass)

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -2,73 +2,51 @@ import fs from 'fs';
 // import path from 'path';
 import gulp from 'gulp';
 
-// Load all gulp plugins automatically
-// and attach them to the `plugins` object
-import plugins from 'gulp-load-plugins';
-
 // Temporary solution until gulp 4
 // https://github.com/gulpjs/gulp/issues/355
 import runSequence from 'run-sequence';
 
-// import archiver from 'archiver';
-// import glob from 'glob';
-import del from 'del';
 import ssri from 'ssri';
 import modernizr from 'modernizr';
 
+//--- styles ---
+const autoprefixer = require('gulp-autoprefixer');
+const cleanCSS = require('gulp-clean-css');
+//const postCSS = require('gulp-postcss');
+const sass = require('gulp-sass');
+
+//-- js --
+//const concat = require('gulp-concat');
+//const uglify = require('gulp-uglify');
+const jscs = require('gulp-jscs');
+const eslint = require('gulp-eslint');
+
+//-- общее --
+const sourcemaps = require('gulp-sourcemaps');
+const browserSync = require('browser-sync').create();
+
+//-- утилиты --
+const rename = require('gulp-rename');
+const del = require('del');
+const replace = require('gulp-replace');
+//const imagemin = require('gulp-imagemin');
+//const cache = require('gulp-cache');
+const notify = require('gulp-notify');
+const plumber = require('gulp-plumber');
+const gulpif = require('gulp-if');
+
+//--- Переменные
 import pkg from './package.json';
 import modernizrConfig from './modernizr-config.json';
-
-
 const dirs = pkg['h5bp-configs'].directories;
 
 // ---------------------------------------------------------------------
 // | Helper tasks                                                      |
 // ---------------------------------------------------------------------
-
-/*gulp.task('archive:create_archive_dir', () => {
-  fs.mkdirSync(path.resolve(dirs.archive), '0755');
-});
-
-gulp.task('archive:zip', (done) => {
-
-  const archiveName = path.resolve(dirs.archive, `${pkg.name}_v${pkg.version}.zip`);
-  const zip = archiver('zip');
-  const files = glob.sync('**!/!*.*', {
-    'cwd': dirs.dist,
-    'dot': true // include hidden files
-  });
-  const output = fs.createWriteStream(archiveName);
-
-  zip.on('error', (error) => {
-    done();
-    throw error;
-  });
-
-  output.on('close', done);
-
-  files.forEach((file) => {
-
-    const filePath = path.resolve(dirs.dist, file);
-
-    // `zip.bulk` does not maintain the file
-    // permissions, so we need to add files individually
-    zip.append(fs.createReadStream(filePath), {
-      'name': file,
-      'mode': fs.statSync(filePath).mode
-    });
-
-  });
-
-  zip.pipe(output);
-  zip.finalize();
-
-});*/
-
 gulp.task('clean', (done) => {
   del([
     // dirs.archive,
-    dirs.dist
+    dirs.dest
   ]).then(() => {
     done();
   });
@@ -76,21 +54,20 @@ gulp.task('clean', (done) => {
 
 gulp.task('copy', [
   'copy:.htaccess',
-  'copy:index.html',
+  'copy:html',
   'copy:jquery',
-  'copy:license',
-  'copy:main.css',
+  // 'copy:license',
   'copy:misc',
-  'copy:normalize'
+  // 'copy:normalize'
 ]);
 
 gulp.task('copy:.htaccess', () =>
   gulp.src('node_modules/apache-server-configs/dist/.htaccess')
-    .pipe(plugins().replace(/# ErrorDocument/g, 'ErrorDocument'))
-    .pipe(gulp.dest(dirs.dist))
+    .pipe(replace(/# ErrorDocument/g, 'ErrorDocument'))
+    .pipe(gulp.dest(dirs.dest))
 );
 
-gulp.task('copy:index.html', () => {
+gulp.task('copy:html', () => {
   const hash = ssri.fromData(
     fs.readFileSync('node_modules/jquery/dist/jquery.min.js'),
     {algorithms: ['sha256']}
@@ -98,36 +75,23 @@ gulp.task('copy:index.html', () => {
   let version = pkg.devDependencies.jquery;
   let modernizrVersion = pkg.devDependencies.modernizr;
 
-  gulp.src(`${dirs.src}/index.html`)
-    .pipe(plugins().replace(/{{JQUERY_VERSION}}/g, version))
-    .pipe(plugins().replace(/{{MODERNIZR_VERSION}}/g, modernizrVersion))
-    .pipe(plugins().replace(/{{JQUERY_SRI_HASH}}/g, hash.toString()))
-    .pipe(gulp.dest(dirs.dist));
+  gulp.src(`${dirs.src}/*.html`)
+    .pipe(replace(/{{JQUERY_VERSION}}/g, version))
+    .pipe(replace(/{{MODERNIZR_VERSION}}/g, modernizrVersion))
+    .pipe(replace(/{{JQUERY_SRI_HASH}}/g, hash.toString()))
+    .pipe(gulp.dest(dirs.dest));
 });
 
 gulp.task('copy:jquery', () =>
   gulp.src(['node_modules/jquery/dist/jquery.min.js'])
-    .pipe(plugins().rename(`jquery-${pkg.devDependencies.jquery}.min.js`))
-    .pipe(gulp.dest(`${dirs.dist}/js/vendor`))
+    .pipe(rename(`jquery-${pkg.devDependencies.jquery}.min.js`))
+    .pipe(gulp.dest(dirs.dest + dirs.js.lib))
 );
 
 gulp.task('copy:license', () =>
   gulp.src('LICENSE.txt')
-    .pipe(gulp.dest(dirs.dist))
+    .pipe(gulp.dest(dirs.dest))
 );
-
-gulp.task('copy:main.css', () => {
-
-  // const banner = `/*! HTML5 Boilerplate v${pkg.version} | ${pkg.license} License | ${pkg.homepage} */\n\n`;
-
-  gulp.src(`${dirs.src}/css/main.css`)
-    //.pipe(plugins().header(banner))
-    .pipe(plugins().autoprefixer({
-      browsers: ['last 2 versions', 'ie >= 9', '> 1%'],
-      cascade: false
-    }))
-    .pipe(gulp.dest(`${dirs.dist}/css`));
-});
 
 gulp.task('copy:misc', () =>
   gulp.src([
@@ -137,26 +101,26 @@ gulp.task('copy:misc', () =>
 
     // Exclude the following files
     // (other tasks will handle the copying of these files)
-    `!${dirs.src}/css/main.css`,
-    `!${dirs.src}/index.html`
+    `!${dirs.src}/css/**/*`,
+    `!${dirs.src}/*.html`
 
   ], {
 
     // Include hidden files by default
     dot: true
 
-  }).pipe(gulp.dest(dirs.dist))
+  }).pipe(gulp.dest(dirs.dest))
 );
 
 gulp.task('copy:normalize', () =>
   gulp.src('node_modules/normalize.css/normalize.css')
-    .pipe(gulp.dest(`${dirs.dist}/css`))
+    .pipe(gulp.dest(`${dirs.dest}/css`))
 );
 
 gulp.task('modernizr', (done) =>{
 
   modernizr.build(modernizrConfig, (code) => {
-    fs.writeFile(`${dirs.dist}/js/vendor/modernizr-${pkg.devDependencies.modernizr}.min.js`, code, done);
+    fs.writeFile(`${dirs.dest}/js/vendor/modernizr-${pkg.devDependencies.modernizr}.min.js`, code, done);
   });
 
 });
@@ -166,9 +130,9 @@ gulp.task('lint:js', () =>
     'gulpfile.js',
     `${dirs.src}/js/*.js`,
     `${dirs.test}/*.js`
-  ]).pipe(plugins().jscs())
-    .pipe(plugins().eslint())
-    .pipe(plugins().eslint.failOnError())
+  ]).pipe(jscs())
+    .pipe(eslint())
+    .pipe(eslint.failOnError())
 );
 
 
@@ -176,19 +140,66 @@ gulp.task('lint:js', () =>
 // | Main tasks                                                        |
 // ---------------------------------------------------------------------
 
-/*gulp.task('archive', (done) => {
-  runSequence(
-    'build',
-    'archive:create_archive_dir',
-    'archive:zip',
-    done);
-});*/
-
 gulp.task('build', (done) => {
   runSequence(
     ['clean', 'lint:js'],
-    'copy', 'modernizr',
+    'sass','copy', 'modernizr',
     done);
+});
+
+gulp.task('sass', function () {
+
+    let isErrorSass = false;
+    let onError = function (err) {
+        isErrorSass = true;
+        notify.onError({
+            title: "Gulp",
+            subtitle: "Failure!",
+            message: "Error: <%= error.message %>",
+            sound: "Beep"
+        })(err);
+
+        this.emit('end');
+    };
+
+    return gulp.src(dirs.src + dirs.precss.src_files)
+        .pipe(plumber({errorHandler: onError}))
+        .pipe(sourcemaps.init())
+        .pipe(sass({outputStyle: 'expanded'}))
+        .pipe(rename({suffix: '.min', prefix: ''}))
+        .pipe(autoprefixer(['last 15 versions']))
+        // .pipe(cleanCSS({
+        //     level: 2
+        // })) // Опционально, закомментировать при отладке
+        .pipe(sourcemaps.mapSources(function (sourcePath, file) {
+            return '../../' + dirs.src + dirs.precss.src + sourcePath;
+        }))
+        .pipe(sourcemaps.write('.'))
+        .pipe(gulp.dest(dirs.dest + dirs.precss.dest))
+        .pipe(gulpif(isErrorSass, notify({ // Add gulpif here
+            title: 'Gulp',
+            subtitle: 'success',
+            message: 'Sass task',
+            sound: "Pop"
+        })))
+        .pipe(browserSync.stream())
+});
+
+gulp.task('browserSync', function () {
+    browserSync.init({
+        server: {
+            baseDir: dirs.dest
+        },
+        notify: true
+        // tunnel: true,
+        // tunnel: "projectmane", //Demonstration page: http://projectmane.localtunnel.me
+    });
+});
+
+gulp.task('watch', ['copy:html', 'sass', 'browserSync'], function () {
+    gulp.watch(dirs.src + dirs.precss.watch_files, ['sass']);
+    // gulp.watch([conf.src + conf.js.lib + '**/*.js', conf.src + conf.js.src + 'common.js'], ['js']);
+    gulp.watch(dirs.src + dirs.html.watch_files, ['copy:html', browserSync.reload]);
 });
 
 gulp.task('default', ['build']);
